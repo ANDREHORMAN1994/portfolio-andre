af9ebd03f5cfa9aa2a9ec504d2109767789d2f46
ANDREHORMAN1994 <andre.horman1994@hotmail.com>
2023-07-05 20:01:25 -0300

    :recycle: refactor: Atualizando Form

--- src/components/FormContact/Form.tsx
+++ src/components/FormContact/Form.tsx
@@ -1,14 +1,54 @@
+import { useEffect, useState } from 'react';
 import { FormContainer, Input, TextArea } from './styles';
 
 function Form() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+  const [isDisabled, setIsDisabled] = useState(true);
+
+  const handleValidation = () => {
+    const valName = name.length > 0;
+    const valEmail = /^\w+@\w+\.\w+$/gm.test(email);
+    const valMessage = message.length > 0;
+    setIsDisabled(!(valName && valEmail && valMessage));
+  };
+
+  const handleFormSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    const whatsappMessage = `Olá, meu nome é ${name} e tenho uma mensagem para você: ${message}`;
+
+    // Exemplo: abrir uma nova janela com o link do WhatsApp
+    const encodedMessage = encodeURIComponent(whatsappMessage);
+    window.open(`https://wa.me/5583993638760?text=${encodedMessage}`);
+  };
+
+  useEffect(handleValidation, [name, email, message]);
+
   return (
-    <FormContainer data-aos="fade-up">
-      <Input type="text" placeholder="Nome" required />
-      <Input type="email" placeholder="E-mail" required />
-      <TextArea placeholder="Mensagem" required />
-      <button type="submit" disabled>
+    <FormContainer data-aos="fade-up" onSubmit={handleFormSubmit}>
+      <Input
+        type="text"
+        placeholder="Nome"
+        onChange={({ target }) => setName(target.value)}
+        required
+      />
+      <Input
+        type="email"
+        placeholder="E-mail"
+        onChange={({ target }) => setEmail(target.value)}
+        required
+      />
+      <TextArea
+        placeholder="Mensagem"
+        onChange={({ target }) => setMessage(target.value)}
+        required
+      />
+      <button type="submit" disabled={isDisabled}>
         Enviar
       </button>
+      {/* <WhatsAppWidget phoneNumber="5583993638760" /> */}
     </FormContainer>
   );
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
-import { ThemeProvider } from 'styled-components';
-
+import NextProgress from 'nextjs-progressbar';
 import { useState } from 'react';
+import { ThemeProvider } from 'styled-components';
 import GlobalStyles from '../styles/global';
 import { themeDark, themeLight } from '../styles/theme';
 
@@ -9,6 +9,13 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <ThemeProvider theme={status ? themeLight : themeDark}>
+      <NextProgress
+        color={status ? themeLight.primary : themeDark.primary}
+        startPosition={0.3}
+        stopDelayMs={200}
+        height={3}
+        showOnShallow
+      />
       <Component {...pageProps} status={status} setStatus={setStatus} />
       <GlobalStyles />
     </ThemeProvider>
